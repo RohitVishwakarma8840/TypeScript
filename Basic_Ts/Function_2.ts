32ee897192eac7a80680a2701fb4506a56d18120
Rohit Vishwakarma <128726730+RohitVishwakarma8840@users.noreply.github.com>
2025-02-19 13:52:19 +0530

    Create Function_2.ts

---
+++ Basic_Ts/Function_2.ts
@@ -0,0 +1,14 @@
+function greet(firstName:string){
+
+    console.log("Hello",firstName);
+}
+
+greet("Rohit");
+
+// function to add 2 numbers in ts
+function add(a:number,b:number){
+    return a+b;
+}
+
+let z:number = add(12,34);
+console.log(z);
