1d1c9a2b1608dcc02a768c8073efb5e1cf89f697
Rohit Vishwakarma <128726730+RohitVishwakarma8840@users.noreply.github.com>
2025-02-19 13:43:44 +0530

    Create First.ts

---
+++ Intro/First.ts
@@ -0,0 +1,16 @@
+// Welcome to the TypeScript World 
+// here comes the type safety
+
+
+const x:number = 1;
+console.log(x);
+
+const y:string = "Rohit";
+
+
+let a:number = 3;
+let b:number = 5;
+
+console.log(a+b);
+console.log(y);
+
